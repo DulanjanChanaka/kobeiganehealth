3848512aa217e2dd2aa5b9e9534512e4ee4dd80e
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-13 23:06:10 +0530

    contact srction add padding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <>
      <Headline title='අප ගැන '/>
 
-    <div className=' p-5 lg:px-[230px] w-full'>
+    <div className=' p-5 lg:px-[270px] w-full'>
         <div className=' lg:flex flex-row  w-full   shadow-xl backdrop-blur-sm bg-white/70  drop-shadow-md rounded-lg '>
             <div className='p-5 flex flex-col justify-center lg:w-3/5'>
                 <p className='text-xs '>DEVISION</p>
@@ -16,8 +16,8 @@ const About = () => {
                 <p className='  text-base pt-10 pb-5 font-light'> බොරලුවැව සෞක්‍ය බල ප්‍රදේශයේ ජනතාව සඳහා ගුණාත්මක හා සේවාවන් කාර්යක්ෂමව ලබා දීම අපගේ අරමුණයි. මෙම web අඩවිය ඔබට සායන තොරතුරු , සෞක්‍ය අද්‍යාපනික තොරතුරු ඇතුළු පහසුකම් රැසක් ඔබට ලබා දී ඇත. ඔබගේ ගැටළු යොමු කිරීම හා සේවාවේ ගුණාත්මකභාවය ඉහල නැංවීම සඳහා ඔබගේ අදහස්ද අපට බොහෝ සෙයින් අගයයි . නිවැරදි තොරතුරු , නිවැරදි වෙලාවට ඔබට පහසුවෙන් ලබා ගත හැක .</p>
                 
             </div>
-            <div className='lg:w-2/5 w-full h-full '>
-                <Image src='/Assets/about.jpg' width='540' height='405' alt="about" />
+            <div className='lg:w-2/5  w-full h-full '>
+                <Image src='/Assets/about.jpg'  width='540' height='405' alt="about" />
             </div>
         </div>
         
