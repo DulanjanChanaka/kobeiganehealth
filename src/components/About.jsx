ec21e1bd13f769f4d1eacdf03f73f348567c8a9d
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-13 18:25:38 +0530

    header bold

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,24 +6,9 @@ const About = () => {
   return (
     <>
      <Headline title='අප ගැන '/>
-    {/* <div id='about'className=' w-full  p-2 flex items-start pb-10 p-1 '>
-       
-        <div className='w-full m-auto md:grid grid-cols-3 gap-8 '>
-            <div className='col-span-2'>
-                
-               
-                <p className='py-2 text-gray-600 text-center '>
-                  බොරලුවැව සෞක්‍ය ප්‍රදේශයේ ජනතාව සඳහා ගුණාත්මක හා සේවාවන් කාර්යක්ෂමව ලබා දීම අපගේ අරමුණයි. මෙම web අඩවිය ඔබට සායන තොරතුරු , සෞක්‍ය අද්‍යාපනික තොරතුරු ඇතුළු පහසුකම් රැසක් ඔබට ලබා දී ඇත. ඔබගේ ගැටළු යොමු කිරීම හා සේවාවේ ගුණාත්මකභාවය ඉහල නැංවීම සඳහා ඔබගේ අදහස්ද අපට බොහෝ සෙයින් අගයයි . නිවැරදි තොරතුරු , නිවැරදි වෙලාවට ඔබට පහසුවෙන් ලබා ගත හැක .
 
-</p>
-            </div>
-            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                <img className='rounded-xl' src='https://i.ibb.co/qdxbJBd/Whats-App-Image-2023-11-04-at-21-49-33-a7bbab1e.jpg' alt="Description of the image" ></img>
-            </div>
-        </div>
-    </div> */}
-    <div className=' p-5 lg:px-[100px]'>
-        <div className=' lg:flex flex-row   shadow-xl backdrop-blur-sm bg-white/70  drop-shadow-md rounded-lg'>
+    <div className=' p-5 lg:px-[100px] '>
+        <div className=' lg:flex flex-row   shadow-xl backdrop-blur-sm bg-white/70  drop-shadow-md rounded-lg '>
             <div className='p-5 flex flex-col justify-center lg:w-3/5'>
                 <p className='text-xs '>DEVISION</p>
                 <h1 className='text-2xl pb-3 font-medium '>KOBEIGANE  MOH</h1>
--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -4,7 +4,7 @@ const Headline = ({title}) => {
   return (
     <div className='flex gap-5  items-center justify-center '>
         <div className='bg-green-600 w-[80px] h-[2px]'></div>
-        <h1 className='text-gray-600 uppercase text-2xl'>{title}</h1>
+        <h1 className='text-gray-600 uppercase text-2xl font-bold'>{title}</h1>
         <div className='bg-green-600 w-[80px] h-[2px]'></div>
     </div>
   )
