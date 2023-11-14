97e9dce793426c90b0d8407655ee986f3eefea8e
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-14 22:54:58 +0530

    test 1

--- src/app/weight/page.jsx
+++ src/app/weight/page.jsx
@@ -1,140 +1,140 @@
-// "use client"
-// import Image from 'next/image'
-// import Link from 'next/link'
-// import React, { useEffect, useState } from 'react';
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
 
-// import { collection, doc, getDocs, query, where } from 'firebase/firestore';
+import { collection, doc, getDocs, query, where } from 'firebase/firestore';
 
-// import { db } from '../../../firebase/firebase';
-// import Navbar from '@/components/Navbar';
+import { db } from '../../../firebase/firebase';
+import Navbar from '@/components/Navbar';
 
-// function Weight() {
-
-
-//     const [personPost, setPersonPost] = useState([]);
-//     const [selectedPost, setSelectedPost] = useState(null);
-//     const [modalVisible, setModalVisible] = useState(false);
+function Weight() {
 
 
-
-//     useEffect(() => {
-//         const fetchPersonPost = async () => {
-
-//             try {
-//                 const returnPostQuery = query(collection(db, 'weight'));
-//                 const returnPostCollection = await getDocs(returnPostQuery);
-//                 const returnPostData = returnPostCollection.docs.map((doc) => ({
-//                     id: doc.id,
-//                     ...doc.data(),
-//                 }));
-//                 setPersonPost(returnPostData);
-//             } catch (error) {
-//                 console.error('Error fetching item posts:', error);
-//             }
-//         };
-//         fetchPersonPost();
-//     }, []);
+    const [personPost, setPersonPost] = useState([]);
+    const [selectedPost, setSelectedPost] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
 
 
 
-//     return (
-//         <div>
-//             <div className="top-0 left-0 sticky">
-//                 <Navbar />
-//             </div>
-//             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 pt-[90px] md:pt-[100px] lg:grid-cols-4 gap-4 p-4">
-//                 {personPost.map((item) => {
-//                     // Check if the item's date is equal to today's date
+    useEffect(() => {
+        const fetchPersonPost = async () => {
+
+            try {
+                const returnPostQuery = query(collection(db, 'weight'));
+                const returnPostCollection = await getDocs(returnPostQuery);
+                const returnPostData = returnPostCollection.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                setPersonPost(returnPostData);
+            } catch (error) {
+                console.error('Error fetching item posts:', error);
+            }
+        };
+        fetchPersonPost();
+    }, []);
 
 
-//                     return (
-//                         <div key={item.id} className="bg-white shadow-lg rounded-lg overflow-hidden ">
-//                             <div className='text-center pt-5'>
-//                             <h3>{item.title}</h3>
-//                             </div>
+
+    return (
+        <div>
+            <div className="top-0 left-0 sticky">
+                <Navbar />
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 pt-[90px] md:pt-[100px] lg:grid-cols-4 gap-4 p-4">
+                {personPost.map((item) => {
+                    // Check if the item's date is equal to today's date
+
+
+                    return (
+                        <div key={item.id} className="bg-white shadow-lg rounded-lg overflow-hidden ">
+                            <div className='text-center pt-5'>
+                            <h3>{item.title}</h3>
+                            </div>
                            
 
-//                             <div className="p-6">
-//                                 <p>{item.january}</p>
-//                                 <button
-//                                     onClick={() => {
-//                                         setSelectedPost(item);
-//                                         setModalVisible(true);
-//                                     }}
-//                                     className="bg-blue-500 hover-bg-blue-600 text-white py-2 px-4 mt-4 rounded"
-//                                 >
-//                                     View Details
-//                                 </button>
-//                             </div>
-//                         </div>
-//                     )
-//                 })}
-//             </div>
+                            <div className="p-6">
+                                <p>{item.january}</p>
+                                <button
+                                    onClick={() => {
+                                        setSelectedPost(item);
+                                        setModalVisible(true);
+                                    }}
+                                    className="bg-blue-500 hover-bg-blue-600 text-white py-2 px-4 mt-4 rounded"
+                                >
+                                    View Details
+                                </button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
 
 
-//             {modalVisible && selectedPost && (
-//                 <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center ">
-//                     <div className="modal-bg fixed top-0 left-0 w-full h-full bg-black opacity-50 "></div>
-//                     <div className="modal-container bg-white w-3/4 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto pb-5">
-//                         <div className="modal-content py-4 text-left px-6 ">
+            {modalVisible && selectedPost && (
+                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center ">
+                    <div className="modal-bg fixed top-0 left-0 w-full h-full bg-black opacity-50 "></div>
+                    <div className="modal-container bg-white w-3/4 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto pb-5">
+                        <div className="modal-content py-4 text-left px-6 ">
                            
-//                             <div className='text-center py-3'> 
-//                             <h3>{selectedPost.title}</h3>
-//                             </div>
+                            <div className='text-center py-3'> 
+                            <h3>{selectedPost.title}</h3>
+                            </div>
 
 
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 january: {selectedPost.january}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 february: {selectedPost.february}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 march: {selectedPost.march}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 april: {selectedPost.april}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 may: {selectedPost.may}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 june: {selectedPost.march}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 july: {selectedPost.july}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 august: {selectedPost.august}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 september: {selectedPost.september}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 october: {selectedPost.october}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3">
-//                                 november: {selectedPost.november}
-//                             </p>
-//                             <p className="text-gray-600 text-sm py-3 mb-5">
-//                                 december: {selectedPost.december}
-//                             </p>
-//                             <div className=''>
+                            <p className="text-gray-600 text-sm py-3">
+                                january: {selectedPost.january}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                february: {selectedPost.february}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                march: {selectedPost.march}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                april: {selectedPost.april}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                may: {selectedPost.may}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                june: {selectedPost.march}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                july: {selectedPost.july}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                august: {selectedPost.august}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                september: {selectedPost.september}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                october: {selectedPost.october}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3">
+                                november: {selectedPost.november}
+                            </p>
+                            <p className="text-gray-600 text-sm py-3 mb-5">
+                                december: {selectedPost.december}
+                            </p>
+                            <div className=''>
 
-//                             <button
-//                                 onClick={() => setModalVisible(false)}
-//                                 className=" bg-red-600 hover:bg-gray-400 text-gray-800 py-1 px-3 rounded float-right"
-//                             >
-//                                 Close
-//                             </button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             )}
-//         </div>
-//     );
-// }
+                            <button
+                                onClick={() => setModalVisible(false)}
+                                className=" bg-red-600 hover:bg-gray-400 text-gray-800 py-1 px-3 rounded float-right"
+                            >
+                                Close
+                            </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
 
 
-// export default Weight
+export default Weight
