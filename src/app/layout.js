f4244b1720f81f06360ce6a6dfd044d16791dfb6
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-16 01:13:05 +0530

    error fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,55 +1,52 @@
+import Head from 'next/head'
+import './globals.css'
+import { Inter } from 'next/font/google'
 
-import Head from 'next/head';
-import { useState, useEffect } from 'react';
-import './globals.css';
-import { Inter } from 'next/font/google';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Kobeigane Health',
   description: 'Kobeigane Public Health division',
-};
-
-const Loader = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      {/* Add your loader component here */}
-      <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
-    </Box>
-    </div>
-  );
-};
+}
 
 export default function RootLayout({ children }) {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate an asynchronous action (e.g., data fetching)
-    const fakeAsyncAction = () => {
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 1000); // Simulating a 2-second delay, replace this with your actual data fetching logic
-    };
-
-    fakeAsyncAction();
-  }, []); // Run this effect only once, similar to componentDidMount
-
   return (
     <html lang="en">
+      
+
       <Head>
+     
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap"
-          rel="stylesheet"
-        ></link>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet"></link>
       </Head>
+        
+      
 
       <body className={inter.className}>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FGHES7MCL7"></script>
+
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-FGHES7MCL7"></script>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FGHES7MCL7');
+            `,
+          }}
+        />
+        
+        {children}</body>
+    </html>
+  )
+}
+
+
+{/* <body className={inter.className}>
+        {children}
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-87VL8427MW"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -60,10 +57,7 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+      </body> */}
 
-        {isLoading ? <Loader /> : children}
-      </body>
-    </html>
-  );
-}
 
+// <!-- Google tag (gtag.js) -->
