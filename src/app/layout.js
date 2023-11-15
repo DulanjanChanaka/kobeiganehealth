5eee939b8c658831da2bb29cc267ceeb97f3c386
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-15 18:14:53 +0530

    font change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      
+
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Iskoola+Pota&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+        
+      
 
       <body className={inter.className}>
 
