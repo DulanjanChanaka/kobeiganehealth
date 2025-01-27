c9afa482bc4e8343cc54f021c62611208c40a633
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2025-01-27 19:43:26 +0530

    add changes 124

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,65 +1,30 @@
 "use client";
-import Head from 'next/head'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { Provider } from 'react-redux'
-import store from '@/redux/store'
+
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Kobeigane Health',
-  description: 'Kobeigane Public Health division',
-}
+  title: "Kobeigane Health",
+  description: "Kobeigane Public Health Division",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-
-      <Head>
-        
-     
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet"></link>
-      </Head>
-        
-      
-
       <body className={inter.className}>
-
-      <Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-FGHES7MCL7"
-  strategy="afterInteractive"
-/>;
-<Script
-  id="google-analytics"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-FGHES7MCL7');
-    `,
-  }}
-/>;
-        
-        <Provider store={store}>
-          {children}
-        </Provider></body>
-    </html>
-  )
-}
-
-
-{/* <body className={inter.className}>
-        {children}
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-87VL8427MW"></script>
-        <script
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FGHES7MCL7"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
@@ -69,7 +34,10 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-      </body> */}
 
-
-// <!-- Google tag (gtag.js) -->
+        {/* Redux Provider */}
+        <Provider store={store}>{children}</Provider>
+      </body>
+    </html>
+  );
+}
